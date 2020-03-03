861eb5f4f7ab50452bed9f579611eb6bad4a941e
Michael V. Gritsenko <payterms@mail.ru>
2020-03-03 18:39:05 +0300

    freeConfig build fixed

--- app/src/main/java/ru/payts/weatherforecastx/ui/send/SendViewModel.java
+++ app/src/main/java/ru/payts/weatherforecastx/ui/send/SendViewModel.java
@@ -10,7 +10,7 @@ public class SendViewModel extends ViewModel {
 
     public SendViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is send fragment");
+        mText.setValue("Just send email at payterms@mail.ru");
     }
 
     public LiveData<String> getText() {
